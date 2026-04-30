49b8bec3ae186cd7002a664d98fdc1a5e7647b7f
Kimberly Zapata <kzapatae@uamv.edu.ni>
2026-04-30 00:36:56 -0600

    HomeScreen Implementado

--- app/src/main/java/com/example/toad/ui/screens/App.kt
+++ app/src/main/java/com/example/toad/ui/screens/App.kt
@@ -5,17 +5,17 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+
 import androidx.compose.ui.Modifier
 
 @Composable
 fun App(modifier: Modifier){
     var logged by remember { mutableStateOf(false) }
+
     Box(modifier = Modifier.fillMaxSize()){
+
+        // LOGIN
         AnimatedVisibility(
             visible = !logged,
             enter = fadeIn(),
@@ -23,12 +23,24 @@ fun App(modifier: Modifier){
         ){
             LoginScreen(onLogin = { logged = true })
         }
+
+        // HOME
         AnimatedVisibility(
             visible = logged,
             enter = fadeIn(),
             exit = fadeOut()
         ){
-            HomeScreen(onLogout = { logged = false })
+            HomeScreen(
+                onLogout = { logged = false },
+
+                onCreateReport = {
+                    println("Ir a crear reporte")
+                },
+
+                onViewDashboard = {
+                    println("Ir al dashboard")
+                }
+            )
         }
     }
 }
--- app/src/main/java/com/example/toad/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/toad/ui/screens/HomeScreen.kt
@@ -1,34 +1,92 @@
 package com.example.toad.ui.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.toad.ui.theme.GreenPrimary
+import com.example.toad.ui.theme.GreenSecundary
+import com.example.toad.ui.theme.White
 
 @Composable
-fun HomeScreen(onLogout: () -> Unit){
+fun HomeScreen(
+    onLogout: () -> Unit,
+    onCreateReport: () -> Unit,
+    onViewDashboard: () -> Unit
+){
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        GreenPrimary,
+                        GreenSecundary
+                    )
+                )
+            ),
         contentAlignment = Alignment.Center
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Bienvenido a Toad",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold)
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = onLogout) {
-                Text(text = "Cerrar Sesión")
+    ){
+        Card(
+            shape = RoundedCornerShape(24.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            elevation = CardDefaults.cardElevation(8.dp)
+        ){
+            Column(
+                modifier = Modifier.padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(
+                    text = "Bienvenido a Toad",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = "Esta aplicación te permite reportar situaciones ambientales de forma rápida y sencilla. Con tu ayuda, es posible generar conciencia y contribuir al cuidado del entorno, promoviendo acciones responsables dentro de la comunidad universitaria.",
+                    fontSize = 14.sp
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Button(
+                    onClick = onCreateReport,
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = GreenPrimary)
+                ) {
+                    Text("Crear Reporte", color = White)
+                }
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Button(
+                    onClick = onViewDashboard,
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = GreenPrimary)
+                ) {
+                    Text("Ver Dashboard", color = White)
+                }
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Button(
+                    onClick = onLogout,
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = GreenPrimary)
+                ) {
+                    Text("Cerrar Sesión", color = White)
+                }
             }
         }
     }
